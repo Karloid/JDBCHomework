08162af6f8dd3e998d17abb1abd8acff677be72f
karloid <krldboyan@gmail.com>
2014-08-22 03:25:02 +0400

    work in progress

--- src/TestDrive.java
+++ src/TestDrive.java
@@ -1,12 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Created by Andrey on 8/22/2014.
  */
 public class TestDrive {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         testJDBC();
     }
 
-    private static void testJDBC() {
+    private static void testJDBC() throws Exception {
         System.out.println("TODO test JDBC...");
+        Class.forName("com.krld.jdbchw.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:hw:test");
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from soldiers");
+        while (rs.next()) {
+            System.out.println("name = "  + rs.getString("name"));
+        }
+        rs.close();
+        conn.close();
     }
 }
