e4cd2c33f8944499162d0a29c576395e13d839ec
karloid <krldboyan@gmail.com>
2014-08-22 03:12:36 +0400

    Initial commit

---
+++ src/TestDrive.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andrey on 8/22/2014.
+ */
+public class TestDrive {
+    public static void main(String[] args) {
+        testJDBC();
+    }
+
+    private static void testJDBC() {
+        System.out.println("TODO test JDBC...");
+    }
+}
